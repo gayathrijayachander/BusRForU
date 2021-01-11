8567de71f36e80ed04cda076612e785f66a171fe
saiteja gajula <saitejagajula.123@gmail.com>
2021-01-11 21:00:25 +0530

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
+  email : string;
+  id=100000;
   constructor() { }
 
   ngOnInit(): void {
